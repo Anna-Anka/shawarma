6f7a7f305a176d0efa445db1dd287eaee0df1e50
Anna-Anka <anjbekrenewa@gmail.com>
2022-11-23 13:02:41 +0300

    corrected some moments

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 const {
-  src,
-  dest,
-  series,
-  watch
+    src,
+    dest,
+    series,
+    watch
 } = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
 const concat = require('gulp-concat');
@@ -25,7 +25,7 @@ const gulpif = require('gulp-if');
 const notify = require('gulp-notify');
 const image = require('gulp-imagemin');
 const {
-  readFileSync
+    readFileSync
 } = require('fs');
 const typograf = require('gulp-typograf');
 const webp = require('gulp-webp');
@@ -43,22 +43,22 @@ const ghPages = require('gh-pages');
 const srcFolder = './src';
 const buildFolder = './app';
 const paths = {
-  srcSvg: `${srcFolder}/img/svg/**.svg`,
-  srcImgFolder: `${srcFolder}/img`,
-  buildImgFolder: `${buildFolder}/img`,
-  srcScss: `${srcFolder}/scss/**/*.scss`,
-  buildCssFolder: `${buildFolder}/css`,
-  srcFullJs: `${srcFolder}/js/**/*.js`,
-  srcMainJs: `${srcFolder}/js/main.js`,
-  buildJsFolder: `${buildFolder}/js`,
-  srcPartialsFolder: `${srcFolder}/partials`,
-  resourcesFolder: `${srcFolder}/resources`,
+    srcSvg: `${srcFolder}/img/svg/**.svg`,
+    srcImgFolder: `${srcFolder}/img`,
+    buildImgFolder: `${buildFolder}/img`,
+    srcScss: `${srcFolder}/scss/**/*.scss`,
+    buildCssFolder: `${buildFolder}/css`,
+    srcFullJs: `${srcFolder}/js/**/*.js`,
+    srcMainJs: `${srcFolder}/js/main.js`,
+    buildJsFolder: `${buildFolder}/js`,
+    srcPartialsFolder: `${srcFolder}/partials`,
+    resourcesFolder: `${srcFolder}/resources`,
 };
 
 let isProd = false; // dev by default
 
 const clean = () => {
-  return del([buildFolder])
+    return del([buildFolder])
 }
 
 const deploy = (cb) => {
@@ -79,257 +79,257 @@ const fonts = () => {
 
 //svg sprite
 const svgSprites = () => {
-  return src(paths.srcSvg)
-    .pipe(
-      svgmin({
-        js2svg: {
-          pretty: true,
-        },
-      })
-    )
-    .pipe(
-      cheerio({
-        run: function ($) {
-          $('[fill]').removeAttr('fill');
-          $('[stroke]').removeAttr('stroke');
-          $('[style]').removeAttr('style');
-        },
-        parserOptions: {
-          xmlMode: true
-        },
-      })
-    )
-    .pipe(replace('&gt;', '>'))
-    .pipe(svgSprite({
-      mode: {
-        stack: {
-          sprite: "../sprite.svg"
-        }
-      },
-    }))
-    .pipe(dest(paths.buildImgFolder));
+    return src(paths.srcSvg)
+        .pipe(
+            svgmin({
+                js2svg: {
+                    pretty: true,
+                },
+            })
+        )
+        .pipe(
+            cheerio({
+                run: function ($) {
+                    $('[fill]').removeAttr('fill');
+                    $('[stroke]').removeAttr('stroke');
+                    $('[style]').removeAttr('style');
+                },
+                parserOptions: {
+                    xmlMode: true
+                },
+            })
+        )
+        .pipe(replace('&gt;', '>'))
+        .pipe(svgSprite({
+            mode: {
+                stack: {
+                    sprite: "../sprite.svg"
+                }
+            },
+        }))
+        .pipe(dest(paths.buildImgFolder));
 }
 
 // scss styles
 const styles = () => {
-  return src('src/scss/style.scss')
-    .pipe(scss({
-        outputStyle: 'compressed'
-    }))
-    .pipe(concat('style.min.css'))
-    .pipe(autoprefixer({
-      grid: true,
-      overrideBrowserslist: ["last 10 versions"]
-    }))
-    .pipe(dest('app/css'))
-    .pipe(browserSync.stream());
+    return src('src/scss/style.scss')
+        .pipe(scss({
+            outputStyle: 'compressed'
+        }))
+        .pipe(concat('style.min.css'))
+        .pipe(autoprefixer({
+            grid: true,
+            overrideBrowserslist: ["last 10 versions"]
+        }))
+        .pipe(dest('app/css'))
+        .pipe(browserSync.stream());
 };
 
 // styles backend
 const stylesBackend = () => {
-  return src(paths.srcScss)
-    .pipe(scss())
-    .pipe(concat('style.min.css'))
-    .pipe(autoprefixer({
-      grid: true,
-      overrideBrowserslist: ["last 10 versions"]
-    }))
-    .pipe(dest('app/css'))
-    .pipe(browserSync.stream());
+    return src(paths.srcScss)
+        .pipe(scss())
+        .pipe(concat('style.min.css'))
+        .pipe(autoprefixer({
+            grid: true,
+            overrideBrowserslist: ["last 10 versions"]
+        }))
+        .pipe(dest('app/css'))
+        .pipe(browserSync.stream());
 };
 
 // scripts
 const scripts = () => {
-  return src(paths.srcMainJs)
-    .pipe(plumber(
-      notify.onError({
-        title: "JS",
-        message: "Error: <%= error.message %>"
-      })
-    ))
-    .pipe(webpackStream({
-      mode: isProd ? 'production' : 'development',
-      output: {
-        filename: 'main.js',
-      },
-      module: {
-        rules: [{
-          test: /\.m?js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: [
-                ['@babel/preset-env', {
-                  targets: "defaults"
+    return src(paths.srcMainJs)
+        .pipe(plumber(
+            notify.onError({
+                title: "JS",
+                message: "Error: <%= error.message %>"
+            })
+        ))
+        .pipe(webpackStream({
+            mode: isProd ? 'production' : 'development',
+            output: {
+                filename: 'main.js',
+            },
+            module: {
+                rules: [{
+                    test: /\.m?js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                ['@babel/preset-env', {
+                                    targets: "defaults"
+                                }]
+                            ]
+                        }
+                    }
                 }]
-              ]
-            }
-          }
-        }]
-      },
-      devtool: !isProd ? 'source-map' : false
-    }))
-    .on('error', function (err) {
-      console.error('WEBPACK ERROR', err);
-      this.emit('end');
-    })
-    .pipe(dest(paths.buildJsFolder))
-    .pipe(browserSync.stream());
+            },
+            devtool: !isProd ? 'source-map' : false
+        }))
+        .on('error', function (err) {
+            console.error('WEBPACK ERROR', err);
+            this.emit('end');
+        })
+        .pipe(dest(paths.buildJsFolder))
+        .pipe(browserSync.stream());
 }
 
 // scripts backend
 const scriptsBackend = () => {
-  return src(paths.srcMainJs)
-    .pipe(plumber(
-      notify.onError({
-        title: "JS",
-        message: "Error: <%= error.message %>"
-      })
-    ))
-    .pipe(webpackStream({
-      mode: 'development',
-      output: {
-        filename: 'main.js',
-      },
-      module: {
-        rules: [{
-          test: /\.m?js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: [
-                ['@babel/preset-env', {
-                  targets: "defaults"
+    return src(paths.srcMainJs)
+        .pipe(plumber(
+            notify.onError({
+                title: "JS",
+                message: "Error: <%= error.message %>"
+            })
+        ))
+        .pipe(webpackStream({
+            mode: 'development',
+            output: {
+                filename: 'main.js',
+            },
+            module: {
+                rules: [{
+                    test: /\.m?js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                ['@babel/preset-env', {
+                                    targets: "defaults"
+                                }]
+                            ]
+                        }
+                    }
                 }]
-              ]
-            }
-          }
-        }]
-      },
-      devtool: false
-    }))
-    .on('error', function (err) {
-      console.error('WEBPACK ERROR', err);
-      this.emit('end');
-    })
-    .pipe(dest(paths.buildJsFolder))
-    .pipe(browserSync.stream());
+            },
+            devtool: false
+        }))
+        .on('error', function (err) {
+            console.error('WEBPACK ERROR', err);
+            this.emit('end');
+        })
+        .pipe(dest(paths.buildJsFolder))
+        .pipe(browserSync.stream());
 }
 
 const resources = () => {
-  return src(`${paths.resourcesFolder}/**`)
-    .pipe(dest(buildFolder))
+    return src(`${paths.resourcesFolder}/**`)
+        .pipe(dest(buildFolder))
 }
 
 const images = () => {
-  return src([`${paths.srcImgFolder}/**/**.{jpg,jpeg,png,svg}`])
-    .pipe(gulpif(isProd, image([
-      image.mozjpeg({
-        quality: 95,
-        progressive: false
-      }),
-      image.optipng({
-        optimizationLevel: 5
-      }),
-    ])))
-    .pipe(dest(paths.buildImgFolder))
+    return src([`${paths.srcImgFolder}/**/**.{jpg,jpeg,png,svg}`])
+        .pipe(gulpif(isProd, image([
+            image.mozjpeg({
+                quality: 95,
+                progressive: false
+            }),
+            image.optipng({
+                optimizationLevel: 5
+            }),
+        ])))
+        .pipe(dest(paths.buildImgFolder))
 };
 
 const webpImages = () => {
-  return src([`${paths.srcImgFolder}/**/**.{jpg,jpeg,png}`])
-    .pipe(webp())
-    .pipe(dest(paths.buildImgFolder))
+    return src([`${paths.srcImgFolder}/**/**.{jpg,jpeg,png}`])
+        .pipe(webp())
+        .pipe(dest(paths.buildImgFolder))
 };
 
 const avifImages = () => {
-  return src([`${paths.srcImgFolder}/**/**.{jpg,jpeg,png}`])
-    .pipe(avif())
-    .pipe(dest(paths.buildImgFolder))
+    return src([`${paths.srcImgFolder}/**/**.{jpg,jpeg,png}`])
+        .pipe(avif())
+        .pipe(dest(paths.buildImgFolder))
 };
 
 const htmlInclude = () => {
-  return src([`${srcFolder}/html/*.html`])
-    .pipe(fileInclude({
-      prefix: '@',
-      basepath: '@file'
-    }))
-    .pipe(typograf({
-      locale: ['ru', 'en-US']
-    }))
-    .pipe(dest(buildFolder))
-    .pipe(browserSync.stream());
+    return src([`${srcFolder}/html/*.html`])
+        .pipe(fileInclude({
+            prefix: '@',
+            basepath: '@file'
+        }))
+        .pipe(typograf({
+            locale: ['ru', 'en-US']
+        }))
+        .pipe(dest(buildFolder))
+        .pipe(browserSync.stream());
 }
 
 const watchFiles = () => {
-  browserSync.init({
-    server: {
-      baseDir: `${buildFolder}`
-    },
-  });
+    browserSync.init({
+        server: {
+            baseDir: `${buildFolder}`
+        },
+    });
 
-  watch(paths.srcScss, styles);
-  watch(paths.srcFullJs, scripts);
-  watch(`${paths.srchtmlFolder}/html/**/*.html`, htmlInclude);
-  watch(`${srcFolder}/html/**/*.html`, htmlInclude);
-  watch(`${paths.resourcesFolder}/**`, resources);
-  watch(`${paths.srcImgFolder}/**/**.{jpg,jpeg,png,svg}`, images);
-  watch(`${paths.srcImgFolder}/**/**.{jpg,jpeg,png}`, webpImages);
-  watch(`${paths.srcImgFolder}/**/**.{jpg,jpeg,png}`, avifImages);
-  watch(paths.srcSvg, svgSprites);
-  watch(['src/fonts/*.ttf']);
+    watch(paths.srcScss, styles);
+    watch(paths.srcFullJs, scripts);
+    watch(`${paths.srchtmlFolder}/html/**/*.html`, htmlInclude);
+    watch(`${srcFolder}/html/**/*.html`, htmlInclude);
+    watch(`${paths.resourcesFolder}/**`, resources);
+    watch(`${paths.srcImgFolder}/**/**.{jpg,jpeg,png,svg}`, images);
+    watch(`${paths.srcImgFolder}/**/**.{jpg,jpeg,png}`, webpImages);
+    watch(`${paths.srcImgFolder}/**/**.{jpg,jpeg,png}`, avifImages);
+    watch(paths.srcSvg, svgSprites);
+    watch(['src/fonts/*.ttf']);
 }
 
 const cache = () => {
-  return src(`${buildFolder}/**/*.{css,js,svg,png,jpg,jpeg,webp,avif,woff2}`, {
-      base: buildFolder
+    return src(`${buildFolder}/**/*.{css,js,svg,png,jpg,jpeg,webp,avif,woff2}`, {
+        base: buildFolder
     })
-    .pipe(rev())
-    .pipe(revDel())
-    .pipe(dest(buildFolder))
-    .pipe(rev.manifest('rev.json'))
-    .pipe(dest(buildFolder));
+        .pipe(rev())
+        .pipe(revDel())
+        .pipe(dest(buildFolder))
+        .pipe(rev.manifest('rev.json'))
+        .pipe(dest(buildFolder));
 };
 
 const rewrite = () => {
-  const manifest = readFileSync('app/rev.json');
-  src(`${paths.buildCssFolder}/*.css`)
-    .pipe(revRewrite({
-      manifest
-    }))
-    .pipe(dest(paths.buildCssFolder));
-  return src(`${buildFolder}/**/*.html`)
-    .pipe(revRewrite({
-      manifest
-    }))
-    .pipe(dest(buildFolder));
+    const manifest = readFileSync('app/rev.json');
+    src(`${paths.buildCssFolder}/*.css`)
+        .pipe(revRewrite({
+            manifest
+        }))
+        .pipe(dest(paths.buildCssFolder));
+    return src(`${buildFolder}/**/*.html`)
+        .pipe(revRewrite({
+            manifest
+        }))
+        .pipe(dest(buildFolder));
 }
 
 const htmlMinify = () => {
-  return src(`${buildFolder}/**/*.html`)
-    .pipe(htmlmin({
-      collapseWhitespace: true
-    }))
-    .pipe(dest(buildFolder));
+    return src(`${buildFolder}/**/*.html`)
+        .pipe(htmlmin({
+            collapseWhitespace: true
+        }))
+        .pipe(dest(buildFolder));
 }
 
 const zipFiles = (done) => {
-  del.sync([`${buildFolder}/*.zip`]);
-  return src(`${buildFolder}/**/*.*`, {})
-    .pipe(plumber(
-      notify.onError({
-        title: "ZIP",
-        message: "Error: <%= error.message %>"
-      })
-    ))
-    .pipe(zip(`${rootFolder}.zip`))
-    .pipe(dest(buildFolder));
+    del.sync([`${buildFolder}/*.zip`]);
+    return src(`${buildFolder}/**/*.*`, {})
+        .pipe(plumber(
+            notify.onError({
+                title: "ZIP",
+                message: "Error: <%= error.message %>"
+            })
+        ))
+        .pipe(zip(`${rootFolder}.zip`))
+        .pipe(dest(buildFolder));
 }
 
 const toProd = (done) => {
-  isProd = true;
-  done();
+    isProd = true;
+    done();
 };
 
 exports.default = series(clean, fonts, htmlInclude, scripts, styles, resources, images, webpImages, avifImages, svgSprites, watchFiles);
